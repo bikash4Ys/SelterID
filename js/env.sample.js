6a91890856d82d81a4ad251802e53ee5cd432cce
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-01-08 12:01:59 +0900

    update

--- js/env.sample.js
+++ js/env.sample.js
@@ -1,3 +1,4 @@
 const API_REGIST_FACE_URL = 'http://localhost:8000/register';
 const API_RECEPT_FACE_URL = 'http://localhost:8000/recognize';
 const API_RECEPT_FACES_URL = 'http://localhost:8000/recognizes';
+const API_RECEPTS_URL = 'http://localhost/SelterID/api/recepts.php';
